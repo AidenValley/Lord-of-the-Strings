9de9961d1f726067945bccf994e3b761f29a4abc
Aiden Jang <jhyjsh953@gmail.com>
2022-07-24 23:36:36 -0700

    retouched CSS and HTML structures

--- app.js
+++ app.js
@@ -2,22 +2,23 @@
 console.log('lord of the strings');
 
 // Initial creation of the variables of each elements in HTML.
-const tennisCanvas = document.getElementById('game');
+const tennisCanvas = document.querySelector('#game');
 const tennisContext = tennisCanvas.getContext('2d');
-const scoreCourt = document.getElementById('score-screen');
-const resetButton = document.getElementById('resetButton');
+const scoreCourt = document.querySelector('#score-screen');
+const resetButton = document.querySelector('#resetButton');
+
 // canvas size
 const gameWidth = tennisCanvas.width;
 const gameHeight = tennisCanvas.height;
 // ideas of CSS color properties
-const tennisCourt = 'forestgreen';
+const courtBackground = 'forestgreen';
 const racket1Color = 'dark-blue';
 const racket2Color = 'dark-orange';
 const racketBorder = 'black';
 const ballColor = 'yellow';
 const ballBorder = 'black';
 const ballRadius = 12.5;
-const paddleSpeed = 50;
+const racketSpeed = 50;
 let IntervalID;
 // slowest speed set
 let ballspeed = 1;
@@ -47,21 +48,52 @@ let racketTwo = {
 window.addEventListener('keydown', changeDirection);
 resetButton.addEventListener('click', resetGame);
 
-//aa
+
 // function 
 gameStart();
 
-function gameStart(){};
-function nextTick(){};
-function clearCourt(){};
-function drawRackets(){};
+function gameStart(){
+    createTennisBall();
+    nextTick();
+};
+function nextTick(){
+    IntervalID = setTimeout(() => {
+        clearCourt();
+        drawRackets();
+        moveTennisBall();
+        drawTennisBall(tennisBallX, tennisBallY);
+        checkCollision();
+        nextTick();
+    }, 20)
+};
+function clearCourt(){
+    tennisContext.fillStyle = courtBackground;
+    tennisContext.fillRect(0, 0, gameWidth, gameHeight);
+};
+
+// function that represents the two player racket icons in the game
+function drawRackets(){
+    tennisContext.strokeStyle = racketBorder;
+
+    tennisContext.fillStyle = racket1Color;
+    tennisContext.fillRect(racketOne.x, racketOne.y, racketOne.width, racketOne.height);
+    tennisContext.strokeRect(racketOne.x, racketOne.y, racketOne.width, racketOne.height);
+
+    tennisContext.fillStyle = racket2Color;
+    tennisContext.fillRect(racketTwo.x, racketTwo.y, racketTwo.width, racketTwo.height);
+    tennisContext.strokeRect(racketTwo.x, racketTwo.y, racketTwo.width, racketTwo.height);
+};
+
 function createTennisBall(){};
 function moveTennisBall(){};
 function drawTennisBall(){};
 function checkCollision(){};
-function changeDirection(){};
+function changeDirection(event){
+    const keyPress = event.keyCode;
+    console.log(keyPress);
+ };
 function updateScore(){};
-function resetGamer(){}
+function resetGame(){}
 
 tennisCanvas.width = 500;
 tennisCanvas.height = 500;
