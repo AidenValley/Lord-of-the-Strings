135f5eb9994aa04d6de67adebb8f430e706007e2
Aiden Jang <107300143+AidenValley@users.noreply.github.com>
2022-07-24 18:24:13 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -4,11 +4,69 @@ console.log('lord of the strings');
 // Initial creation of the variables of each elements in HTML.
 const tennisCanvas = document.getElementById('game');
 const tennisContext = tennisCanvas.getContext('2d');
+const scoreCourt = document.getElementById('score-screen');
+const resetButton = document.getElementById('resetButton');
+// canvas size
+const gameWidth = tennisCanvas.width;
+const gameHeight = tennisCanvas.height;
+// ideas of CSS color properties
+const tennisCourt = 'forestgreen';
+const racket1Color = 'dark-blue';
+const racket2Color = 'dark-orange';
+const racketBorder = 'black';
+const ballColor = 'yellow';
+const ballBorder = 'black';
+const ballRadius = 12.5;
+const paddleSpeed = 50;
+let IntervalID;
+// slowest speed set
+let ballspeed = 1;
+// tennis ball size
+let tennisBallX = gameWidth / 2;
+let tennisBallY = gameHeight / 2;
+// Ball direction sets
+let tennisBallXDirection = 0;
+let tennisBallYDirection = 0;
+// Initial score sets of a player one and two.
+let playerOneScore = 0;
+let playerTwoScore = 0;
+let racketOne = {
+    width: 25,
+    height: 100,
+    x:0,
+    y:0
+};
+let racketTwo = {
+    width: 25,
+    height: 100,
+    x: gameWidth - 25,
+    y: gameHeight - 100
+};
+
+// addEventlistener function goes here
+window.addEventListener('keydown', changeDirection);
+resetButton.addEventListener('click', resetGame);
+
+
+// function 
+gameStart();
+
+function gameStart(){};
+function nextTick(){};
+function clearCourt(){};
+function drawRackets(){};
+function createTennisBall(){};
+function moveTennisBall(){};
+function drawTennisBall(){};
+function checkCollision(){};
+function changeDirection(){};
+function updateScore(){};
+function resetGamer(){}
+
 tennisCanvas.width = 500;
 tennisCanvas.height = 500;
 
-// Initial score sets of a player one and two.
-let playerOne = 0;
-let playerTwo = 0;
+
+
 
 
