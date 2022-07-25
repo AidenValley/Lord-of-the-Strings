dfe60e87186c952687257ad00672a907f9181b8e
Aiden Jang <107300143+AidenValley@users.noreply.github.com>
2022-07-24 17:06:01 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// First Initial Test
+console.log('lord of the strings');
+
+// Initial creation of the variables of each elements in HTML.
+const tennisCanvas = document.getElementById('game');
+const tennisContext = tennisCanvas.getContext('2d');
+tennisCanvas.width = 500;
+tennisCanvas.height = 500;
+
+// Initial score sets of a player one and two.
+let playerOne = 0;
+let playerTwo = 0;
+
+
